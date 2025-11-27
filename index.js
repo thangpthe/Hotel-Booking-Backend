26dbd569923f107d7a99dd1b7dd96c443255c9ec
thangpt <phungthethang1607@gmail.com>
2025-11-27 22:59:58 +0700

    commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
 import mongoose from "mongoose"; 
 import userRouter from "./routes/user.routes.js";
 import hotelRouter from "./routes/hotel.routes.js";
@@ -11,7 +12,6 @@ import bookingRouter from "./routes/booking.routes.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
-dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
