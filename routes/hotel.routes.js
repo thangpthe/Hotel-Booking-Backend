8485be0b8491ec972c64cbb5b070506ae3122193
thangpt <phungthethang1607@gmail.com>
2025-11-28 21:30:02 +0700

    commit

--- routes/hotel.routes.js
+++ routes/hotel.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { isAuthenticated } from "../middllewares/isAuthenticated.js";
 import {isOwner} from "../middllewares/isOwner.js";
-import { deleteHotel, getAllHotels, getOwnerHotels, registerHotel } from "../controllers/hotel.controller.js";
+import { deleteHotel, getAllHotels, getHotelById, getOwnerHotels, registerHotel } from "../controllers/hotel.controller.js";
 import { upload } from "../config/multer.js";
 const hotelRouter = express.Router();
 hotelRouter.post("/register",upload.single("image"),isAuthenticated,isOwner,registerHotel);
 
 hotelRouter.get("/get",isAuthenticated,isOwner,getOwnerHotels);
 hotelRouter.get("/get-all",getAllHotels);
-router.get("/:id", getHotelById);
+hotelRouter.get("/:id", getHotelById);
 hotelRouter.delete("/delete/:hotelId",isAuthenticated,isOwner,deleteHotel);
 
 export default hotelRouter;
